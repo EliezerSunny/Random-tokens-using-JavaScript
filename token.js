0a06a6209601dba3c4086c2804b52d7f77ff5f2d
Adetunjieliazer <adetunjieliazer@gmail.com>
2023-10-13 15:50:41 +0100

    Random-tokens-using-JavaScript

--- token.js
+++ token.js
@@ -1,13 +1,15 @@
 
   console.log("Random tokens");
 
-    function generateTokens() {
+   // function generateTokens<F11>() {
       const tokens = [];
       for (let i = 0; i < 5; i++) {
         const randomInt = Math.floor(Math.random() * 9999); // Generate random integer between 0 and 9998
         tokens.push(randomInt);
       }
 
-    console.log("Token:" + tokens + tokens.join("-"));
-    }
+    const tokenOutput = tokens.join("-");
+
+    console.log("Token:" + tokenOutput);
+   // }
 
